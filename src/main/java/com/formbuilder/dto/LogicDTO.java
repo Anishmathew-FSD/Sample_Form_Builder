3c204eca6c1070aeda1fea38ecd8e987f9e0fbfc
anish <anish.mathew@piserve.com>
2024-01-19 12:28:35 +0530

    API changes

--- src/main/java/com/formbuilder/dto/LogicDTO.java
+++ src/main/java/com/formbuilder/dto/LogicDTO.java
@@ -1,12 +1,16 @@
 package com.formbuilder.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.List;
 @Data
+@ApiModel(description = "DTO for representing logic")
 public class LogicDTO {
     private String expression;
     private String logicValue;
+    @ApiModelProperty(value = "List of questions affected by this logic")
     private List<QuestionDTO> questionDTOS;
 
 }
